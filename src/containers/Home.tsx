89b6506b2d1d96ca193a46ced92ab789fbc20b78
Altercodex3110 <aarondavidfp@hotmail.com>
2021-05-07 22:16:25 -0700

    Update test
    Update includes:
    1 button
    Delete div Home.

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>Pelota
+            <button>Rojo</button>
+        </div>
     )
 }
 export default Home;
